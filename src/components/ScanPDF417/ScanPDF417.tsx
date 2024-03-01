47e8fb1448f818ea2b1bb01c7f15a6b94619e998
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2024-03-01 10:59:22 -0300

    validateData

--- src/components/ScanPDF417/ScanPDF417.tsx
+++ src/components/ScanPDF417/ScanPDF417.tsx
@@ -22,15 +22,70 @@ export const BarcodeScanner = () => {
     setResult(""); 
     setScanning(false);
   };
-  
+
   const { ref } = useZxing({
-    
     onDecodeResult(result) {
-      setResult(result.getText());
+      const data = result.getText();
+      const dataArray = data.split("@");
+      if (dataArray.length > 0 && /^\d+$/.test(dataArray[0])) {
+        setResult(data);
+        console.log("Validación del 1er dato: correcta, es numérico - Indica N° de Tramite");
+      } else {
+        setResult("El primer dato no es numérico");
+      }
+
+      if (dataArray.length > 1 && /^[a-zA-Z\s]+$/.test(dataArray[1])) {
+        console.log("Validación del 2do dato: correcta, es texto - Indica Apellido");
+      } else {
+        console.log("El 2do dato no es texto");
+      }
+
+      if (dataArray.length > 2 && /^[a-zA-Z\s]+$/.test(dataArray[2])) {
+        console.log("Validación del 3er dato: correcta, es texto - Indica Nombre");
+      } else {
+        console.log("El 3er dato no es texto");
+      }
+
+      if (dataArray.length > 3 && /^[a-zA-Z\s]+$/.test(dataArray[3])) {
+        console.log("Validación del 4to dato: correcta, es texto - Indica Sexo");
+      } else {
+        console.log("El 4to dato no es texto");
+      }
+
+      if (dataArray.length > 4 && /^\d+$/.test(dataArray[4])) {
+        console.log("Validación del 5to dato: correcta, es numérico - Indica DNI");
+      } else {
+        setResult("El 5to dato no es numérico");
+      }
+
+      if (dataArray.length > 5 && /^[a-zA-Z\s]+$/.test(dataArray[5])) {
+        console.log("Validación del 6to dato: correcta, es texto - Indica Ejemplar");
+      } else {
+        console.log("El 6to dato no es texto");
+      }
+
+      if (dataArray.length > 6 && isValidDate(dataArray[6])) {
+        console.log("Validación del 7mo dato: correcta, es una fecha - Indica Fecha de Nacimiento");
+      } else {
+        console.log("El 7mo dato no es una fecha válida");
+      }
+
+      if (dataArray.length > 7 && isValidDate(dataArray[7])) {
+        console.log("Validación del 8vo dato: correcta, es una fecha - Indica Fecha de Emisión");
+      } else {
+        console.log("El 7mo dato no es una fecha válida");
+      }
+      
     },
-    
     paused: !scanning,
   });
+
+  // Función para validar el formato de fecha
+  function isValidDate(dateString: string) {
+    // Expresión regular para validar el formato "dd/mm/aaaa"
+    const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+    return dateRegex.test(dateString);
+  }
 
   return (
     <>
@@ -38,9 +93,9 @@ export const BarcodeScanner = () => {
         <video className="scanVideo" ref={ref} />
 
         {!scanning ? (
-          <button onClick={startScanning}>Iniciar escaneo</button>
+          <button className="btnScan" onClick={startScanning}>Iniciar escaneo</button>
         ) : (
-          <button onClick={resetScanning}>Reiniciar escaneo</button>
+          <button className="btnScan" onClick={resetScanning}>Reiniciar escaneo</button>
         )}
 
         <div className="results">
@@ -58,3 +113,4 @@ export const BarcodeScanner = () => {
     </>
   );
 };
+
