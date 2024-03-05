d11cc6483141007a2a8bcf7db71acd26039ac238
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2024-03-05 18:09:11 -0300

    dddsadfsadfsdfs

--- src/components/ScanPDF417/ScanPDF417.tsx
+++ src/components/ScanPDF417/ScanPDF417.tsx
@@ -1,61 +1,30 @@
-
 import { useState, useEffect, useRef } from "react";
 import { useZxing } from "react-zxing";
-
-
-import "./ScanPDF417.css"
-
-
+import "./ScanPDF417.css";
 
 export const BarcodeScanner = () => {
   const [result, setResult] = useState("");
   const [scanning, setScanning] = useState(false);
   const [maxResolution, setMaxResolution] = useState({ width: 0, height: 0 });
   const [maxResolutionDevice, setMaxResolutionDevice] = useState<MediaDeviceInfo | null>(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const [availableCameras, setAvailableCameras] = useState<MediaDeviceInfo[]>([]);
 
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     navigator.mediaDevices.enumerateDevices()
-      .then(function (devices) {
+      .then(function(devices) {
         console.log('Devices:', devices);
-        var videoDevices = devices.filter(function (device) {
+        const cameras = devices.filter(function(device) {
           return device.kind === 'videoinput';
         });
 
-        videoDevices.forEach(function (device) {
-          navigator.mediaDevices.getUserMedia({
-            video: {
-              deviceId: {
-                exact: device.deviceId
-              },
-              // width: { ideal: 1920 }, // Intenta establecer una resolución más baja
-              // height: { ideal: 1080 },  // Intenta establecer una resolución más baja
-              // Utilizar cámara trasera si está disponible
-            
-
-
-            }
-          })
-            .then(function (stream) {
-              console.log('Stream:', stream);
-              var track = stream.getVideoTracks()[0];
-              var capabilities = track.getCapabilities();
-
-              if (capabilities.width && capabilities.height && capabilities.width.max && capabilities.height.max) {
-                if (capabilities.width.max * capabilities.height.max > maxResolution.width * maxResolution.height) {
-                  setMaxResolution({ width: capabilities.width.max, height: capabilities.height.max });
-                  setMaxResolutionDevice(device);
-                }
-              }
-            });
-        });
+        setAvailableCameras(cameras);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log(err.name + ": " + err.message);
       });
   }, []);
-
 
   const startScanning = () => {
     setResult("");
@@ -68,17 +37,16 @@ export const BarcodeScanner = () => {
         height: { exact: maxResolution.height }
       }
     })
-      .then(function (stream) {
+      .then(function(stream) {
         // Use the stream with the video ref
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log(err.name + ": " + err.message);
       });
   };
-
 
   const resetScanning = () => {
     setResult("");
@@ -87,62 +55,58 @@ export const BarcodeScanner = () => {
 
   const stopScanning = () => {
     setScanning(false);
-
   };
 
   const { ref } = useZxing({
     onDecodeResult(result) {
       const data = result.getText();
       const dataArray = data.split("@");
-      if (dataArray.length > 0 && /^\d+$/.test(dataArray[0])) {
-        setResult(data);
-        console.log("Validación del 1er dato: correcta, es numérico - Indica N° de Tramite");
-      } else {
+      if (dataArray.length !== 8) {
+        setResult("Número incorrecto de datos");
+        return;
+      }
+
+      if (!/^\d+$/.test(dataArray[0])) {
         setResult("El primer dato no es numérico");
+        return;
       }
 
-      if (dataArray.length > 1 && /^[a-zA-Z\s]+$/.test(dataArray[1])) {
-        console.log("Validación del 2do dato: correcta, es texto - Indica Apellido");
-      } else {
-        console.log("El 2do dato no es texto");
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[1])) {
+        setResult("El segundo dato no es texto");
+        return;
       }
 
-      if (dataArray.length > 2 && /^[a-zA-Z\s]+$/.test(dataArray[2])) {
-        console.log("Validación del 3er dato: correcta, es texto - Indica Nombre");
-      } else {
-        console.log("El 3er dato no es texto");
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[2])) {
+        setResult("El tercer dato no es texto");
+        return;
       }
 
-      if (dataArray.length > 3 && /^[a-zA-Z\s]+$/.test(dataArray[3])) {
-        console.log("Validación del 4to dato: correcta, es texto - Indica Sexo");
-      } else {
-        console.log("El 4to dato no es texto");
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[3])) {
+        setResult("El cuarto dato no es texto");
+        return;
       }
 
-      if (dataArray.length > 4 && /^\d+$/.test(dataArray[4])) {
-        console.log("Validación del 5to dato: correcta, es numérico - Indica DNI");
-      } else {
-        setResult("El 5to dato no es numérico");
+      if (!/^\d+$/.test(dataArray[4])) {
+        setResult("El quinto dato no es numérico");
+        return;
       }
 
-      if (dataArray.length > 5 && /^[a-zA-Z\s]+$/.test(dataArray[5])) {
-        console.log("Validación del 6to dato: correcta, es texto - Indica Ejemplar");
-      } else {
-        console.log("El 6to dato no es texto");
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[5])) {
+        setResult("El sexto dato no es texto");
+        return;
       }
 
-      if (dataArray.length > 6 && isValidDate(dataArray[6])) {
-        console.log("Validación del 7mo dato: correcta, es una fecha - Indica Fecha de Nacimiento");
-      } else {
-        console.log("El 7mo dato no es una fecha válida");
+      if (!isValidDate(dataArray[6])) {
+        setResult("El séptimo dato no es una fecha válida");
+        return;
       }
 
-      if (dataArray.length > 7 && isValidDate(dataArray[7])) {
-        console.log("Validación del 8vo dato: correcta, es una fecha - Indica Fecha de Emisión");
-      } else {
-        console.log("El 8vo dato no es una fecha válida");
+      if (!isValidDate(dataArray[7])) {
+        setResult("El octavo dato no es una fecha válida");
+        return;
       }
 
+      setResult(data);
     },
     paused: !scanning,
   });
@@ -160,7 +124,6 @@ export const BarcodeScanner = () => {
         <div className="videoWrapper">
           <video className={window.innerWidth <= 600 ? 'scanVideo smallScreen' : 'scanVideo'} ref={ref} />
           <div className="border"></div>
-
         </div>
         {!scanning ? (
           <button className="btnScan" onClick={startScanning}>Iniciar escaneo</button>
@@ -169,14 +132,20 @@ export const BarcodeScanner = () => {
         )}
 
         <div className="results">
-          <h5>
-            Last result:
-          </h5>
-
+          <h5>Last result:</h5>
+          <p>{result}</p>
           <p>
-            {result}
+            {availableCameras.length > 0 && (
+              <>
+                Available Cameras:
+                <ul>
+                  {availableCameras.map((camera, index) => (
+                    <li key={index}>{camera.label}</li>
+                  ))}
+                </ul>
+              </>
+            )}
           </p>
-
           <p>
             {maxResolutionDevice && (
               <>
@@ -184,13 +153,9 @@ export const BarcodeScanner = () => {
               </>
             )}
           </p>
-
           <button onClick={stopScanning}>Cerrar Scan</button>
         </div>
       </div>
     </>
   );
 };
-
-
-
