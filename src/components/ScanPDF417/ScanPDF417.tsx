7cc3542565a3c37b2f8dbf4762a94d206dc863c7
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2024-03-06 19:00:33 -0300

    update

--- src/components/ScanPDF417/ScanPDF417.tsx
+++ src/components/ScanPDF417/ScanPDF417.tsx
@@ -1,191 +1,104 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { useZxing } from "react-zxing";
 import "./ScanPDF417.css";
+import { useMediaDevices } from "react-media-devices";
 
 export const BarcodeScanner = () => {
   const [result, setResult] = useState("");
   const [scanning, setScanning] = useState(false);
-  const [maxResolution, setMaxResolution] = useState({ width: 0, height: 0 });
-  const [maxResolutionDevice, setMaxResolutionDevice] = useState<MediaDeviceInfo | null>(null);
-  const [availableCameras, setAvailableCameras] = useState<MediaDeviceInfo[]>([]);
-
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices()
-      .then(function(devices) {
-        console.log('Devices:', devices);
-        const cameras = devices.filter(function(device) {
-          return device.kind === 'videoinput';
-        });
-
-        setAvailableCameras(cameras);
-
-        cameras.forEach(function(camera) {
-          navigator.mediaDevices.getUserMedia({
-            video: { deviceId: { exact: camera.deviceId } }
-          })
-            .then(function(stream) {
-              console.log('Stream:', stream);
-              const track = stream.getVideoTracks()[0];
-              const capabilities = track.getCapabilities();
-
-              if (capabilities.width && capabilities.height && capabilities.width.max && capabilities.height.max) {
-                if (capabilities.width.max * capabilities.height.max > maxResolution.width * maxResolution.height) {
-                  setMaxResolution({ width: capabilities.width.max, height: capabilities.height.max });
-                  setMaxResolutionDevice(camera);
-                }
-              }
-            });
-        });
-      })
-      .catch(function(err) {
-        console.log(err.name + ": " + err.message);
-      });
-  }, []);
-
-  const startScanning = () => {
-    setResult("");
-    setScanning(true);
-
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        deviceId: { exact: maxResolutionDevice ? maxResolutionDevice.deviceId : undefined },
-        width: { exact: maxResolution.width },
-        height: { exact: maxResolution.height }
-      }
-    })
-      .then(function(stream) {
-        // Use the stream with the video ref
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      })
-      .catch(function(err) {
-        console.log(err.name + ": " + err.message);
-      });
-  };
-
-  const resetScanning = () => {
-    setResult("");
-    setScanning(true);
-  };
-
-  const stopScanning = () => {
-    setScanning(false);
-  };
+  const { devices } = useMediaDevices({ constraints: { video: true } });
+  const deviceId = devices?.[0]?.deviceId;
 
   const { ref } = useZxing({
     onDecodeResult(result) {
+      console.log("Resultado decodificado:", result.getText());
       const data = result.getText();
-      // const dataArray = data.split("@");
-      // if (dataArray.length !== 8) {
-      //   setResult("Número incorrecto de datos");
-      //   return;
-      // }
+      const dataArray = data.split("@");
+      if (dataArray.length !== 9) {
+        setResult("Número incorrecto de datos");
+        return;
+      }
 
-      // if (!/^\d+$/.test(dataArray[0])) {
-      //   setResult("El primer dato no es numérico");
-      //   return;
-      // }
+      if (!/^\d+$/.test(dataArray[0])) {
+        setResult("El primer dato no es numérico");
+        return;
+      }
 
-      // if (!/^[a-zA-Z\s]+$/.test(dataArray[1])) {
-      //   setResult("El segundo dato no es texto");
-      //   return;
-      // }
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[1])) {
+        setResult("El segundo dato no es texto");
+        return;
+      }
 
-      // if (!/^[a-zA-Z\s]+$/.test(dataArray[2])) {
-      //   setResult("El tercer dato no es texto");
-      //   return;
-      // }
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[2])) {
+        setResult("El tercer dato no es texto");
+        return;
+      }
 
-      // if (!/^[a-zA-Z\s]+$/.test(dataArray[3])) {
-      //   setResult("El cuarto dato no es texto");
-      //   return;
-      // }
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[3])) {
+        setResult("El cuarto dato no es texto");
+        return;
+      }
 
-      // if (!/^\d+$/.test(dataArray[4])) {
-      //   setResult("El quinto dato no es numérico");
-      //   return;
-      // }
+      if (!/^\d+$/.test(dataArray[4])) {
+        setResult("El quinto dato no es numérico");
+        return;
+      }
 
-      // if (!/^[a-zA-Z\s]+$/.test(dataArray[5])) {
-      //   setResult("El sexto dato no es texto");
-      //   return;
-      // }
+      if (!/^[a-zA-Z\s]+$/.test(dataArray[5])) {
+        setResult("El sexto dato no es texto");
+        return;
+      }
 
-      // if (!isValidDate(dataArray[6])) {
-      //   setResult("El séptimo dato no es una fecha válida");
-      //   return;
-      // }
+      if (!isValidDate(dataArray[6])) {
+        setResult("El séptimo dato no es una fecha válida");
+        return;
+      }
 
-      // if (!isValidDate(dataArray[7])) {
-      //   setResult("El octavo dato no es una fecha válida");
-      //   return;
-      // }
+      if (!isValidDate(dataArray[7])) {
+        setResult("El octavo dato no es una fecha válida");
+        return;
+      }
 
       setResult(data);
     },
     paused: !scanning,
+    deviceId: deviceId 
   });
 
-  // // Función para validar el formato de fecha
-  // function isValidDate(dateString: string) {
-  //   // Expresión regular para validar el formato "dd/mm/aaaa"
-  //   const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
-  //   return dateRegex.test(dateString);
-  // }
+  // Función para validar el formato de fecha
+  function isValidDate(dateString: string) {
+    // Expresión regular para validar el formato "dd/mm/aaaa"
+    const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+    return dateRegex.test(dateString);
+  }
+  const stopScanning = () => {
+    console.log("Deteniendo escaneo...");
+    setScanning(false);
+    setResult("");
+  };
+
+  const startScanning = () => {
+    console.log("Iniciando escaneo...");
+    setResult("");
+    setScanning(true);
+  };
 
   return (
     <>
       <div className="scanContent">
         <div className="videoWrapper">
-          <video className={window.innerWidth <= 600 ? 'scanVideo smallScreen' : 'scanVideo'} ref={ref} />
+          <video ref={ref} />
           <div className="border"></div>
         </div>
-        {!scanning ? (
+        {!scanning && (
           <button className="btnScan" onClick={startScanning}>Iniciar escaneo</button>
-        ) : (
-          <button className="btnScan" onClick={resetScanning}>Reiniciar escaneo</button>
         )}
 
         <div className="results">
           <h5>Last result:</h5>
           <p>{result}</p>
-          <p>
-            {availableCameras.length > 0 && (
-              <>
-                Available Cameras:
-                <ul>
-                  {availableCameras.map((camera, index) => (
-                    <li key={index} onClick={() => {
-                      setMaxResolutionDevice(camera);
-                      // Retrieve resolution for selected camera
-                      navigator.mediaDevices.getUserMedia({
-                        video: { deviceId: { exact: camera.deviceId } }
-                      })
-                        .then(function(stream) {
-                          const track = stream.getVideoTracks()[0];
-                          const capabilities = track.getCapabilities();
-                          if (capabilities.width && capabilities.height && capabilities.width.max && capabilities.height.max) {
-                            setMaxResolution({ width: capabilities.width.max, height: capabilities.height.max });
-                          }
-                        });
-                    }}>{camera.label}</li>
-                  ))}
-                </ul>
-              </>
-            )}
-          </p>
-          <p>
-            {maxResolutionDevice && (
-              <>
-                Max Resolution: {maxResolution.width}x{maxResolution.height}
-              </>
-            )}
-          </p>
-          <button onClick={stopScanning}>Cerrar Scan</button>
         </div>
+        <button onClick={stopScanning}>Cerrar Scan</button>
       </div>
     </>
   );
