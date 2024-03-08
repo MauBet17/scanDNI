178dd414013bae3754cd41a405f5ecba1960c543
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2024-03-08 11:33:34 -0300

    act

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import './App.css'
 import { BarcodeScanner } from './components/ScanPDF417/ScanPDF417'
 
 
-// Add a type declaration for ReactCodeScanner component
+
 
 
 const App = () => {
 
-  
+
   return (
     <>
       <div>
